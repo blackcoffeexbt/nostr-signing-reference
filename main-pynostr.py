84534f8e122d461d9cf6265abbf2a27b19ce8d8f
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2024-12-16 13:49:42 +0000

    Added nostr version

---
+++ main-pynostr.py
@@ -0,0 +1,43 @@
+import asyncio
+import json
+import os
+from dotenv import load_dotenv
+from nostr.key import PrivateKey
+from nostr.event import Event, EventKind
+
+# Load Private Key from .env file
+load_dotenv()
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+
+if not PRIVATE_KEY:
+    raise ValueError("PRIVATE_KEY not found in .env file. Please define it.")
+
+async def run():
+    # Initialize Private Key
+    private_key = PrivateKey(bytes.fromhex(PRIVATE_KEY))
+
+    # Build an Event
+    message = "Message"
+    timestamp = 1734354144
+    public_key = private_key.public_key.hex()
+    event = Event(
+        kind=EventKind.TEXT_NOTE,
+        content=message,
+        created_at=timestamp,
+        public_key=public_key
+    )
+
+    # Sign the Event
+    print("Signing event with private key:", PRIVATE_KEY)
+    private_key.sign_event(event)
+
+    # Output Results
+    event_json = event.to_message()
+    print("----------------------------------")
+    print("Signed Event JSON:", event_json)
+    print("Signed event Hash:", event.id)
+    print("Signed event signature:", event.signature)
+    print("----------------------------------")
+
+if __name__ == "__main__":
+    asyncio.run(run())
